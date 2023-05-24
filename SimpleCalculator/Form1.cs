4b094cfc0880975fec15c53faf13b8a1f67e9776
Modder <63869963+ModderMK@users.noreply.github.com>
2023-05-24 14:18:53 -0300

    Att
    Att

--- SimpleCalculator/Form1.cs
+++ SimpleCalculator/Form1.cs
@@ -48,6 +48,12 @@ namespace SimpleCalculator
             richtxtVisor.Refresh();
         }
 
+        private void addTextRichtxtVisor(string text)
+        {
+            richtxtVisor.Text += text;
+            richtxtVisor.Refresh();
+        }
+
         private void btnClick(object sender, EventArgs e)
         {
             var button = (Button)sender;
@@ -57,7 +63,7 @@ namespace SimpleCalculator
 
                     break;
                 case "btnClearEntry":
-
+                    changeRichtxtVisor("");
                     break;
                 case "btnBackspace":
                     
@@ -84,34 +90,34 @@ namespace SimpleCalculator
 
                     break;
                 case "btnNumber0":
-
+                    addTextRichtxtVisor("0");
                     break;
                 case "btnNumber1":
-
+                    addTextRichtxtVisor("1");
                     break;
                 case "btnNumber2":
-
+                    addTextRichtxtVisor("2");
                     break;
                 case "btnNumber3":
-
+                    addTextRichtxtVisor("3");
                     break;
                 case "btnNumber4":
-
+                    addTextRichtxtVisor("4");
                     break;
                 case "btnNumber5":
-
+                    addTextRichtxtVisor("5");
                     break;
                 case "btnNumber6":
-
+                    addTextRichtxtVisor("6");
                     break;
                 case "btnNumber7":
-
+                    addTextRichtxtVisor("7");
                     break;
                 case "btnNumber8":
-
+                    addTextRichtxtVisor("8");
                     break;
                 case "btnNumber9":
-
+                    addTextRichtxtVisor("9");
                     break;
                 default:
                     MessageBox.Show("Object Name:"+buttonName, "ERRO:");
